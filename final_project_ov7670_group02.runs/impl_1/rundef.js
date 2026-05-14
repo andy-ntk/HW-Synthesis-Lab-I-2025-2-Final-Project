24f0c1b51095c5225c9db050d1f4920e912d7b19
andy-ntk <natthakorn.yie@outlook.com>
2026-05-14 09:35:51 +0700

    Revise bitstream

--- final_project_ov7670_group02.runs/impl_1/rundef.js
+++ final_project_ov7670_group02.runs/impl_1/rundef.js
@@ -28,7 +28,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log ov7670_top.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source ov7670_top.tcl -notrace" );
 
--- final_project_ov7670_group02.runs/synth_1/ov7670_top.tcl
+++ final_project_ov7670_group02.runs/synth_1/ov7670_top.tcl
@@ -56,6 +56,7 @@ if {$::dispatch::connected} {
 }
 
 OPTRACE "synth_1" START { ROLLUP_AUTO }
+set_param xicom.use_bs_reader 1
 set_param chipscope.maxJobs 3
 set_param general.usePosixSpawnForFork 1
 OPTRACE "Creating in-memory project" START { }
